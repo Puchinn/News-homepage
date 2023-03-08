5c7a64c3a32d58d406d311dea71c7b7d92e2d648
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 18:06:12 -0300

    update view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,21 @@
+import Nav from './components/Nav'
+import MainContent from './components/MainContent'
+import NewsContent from './components/NewsContent'
+import Interesting from './components/Interesting'
+
 function App() {
 	return (
-		<div className='App'>
-			<h1 className='text-red-500 text-7xl'>Hola mundinho</h1>
+		<div className='App max-w-7xl mx-auto space-y-7'>
+			<Nav />
+			<div className='grid grid-cols-12 gap-x-7'>
+				<div className='col-span-8'>
+					<MainContent />
+				</div>
+				<div className='col-span-4'>
+					<NewsContent />
+				</div>
+				<Interesting />
+			</div>
 		</div>
 	)
 }
