a5277880c30c5fe70cf59910dd4ca01ffb327925
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 18:00:53 -0300

    add styles for active state

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,7 +1,9 @@
 function Article({ titulo, desc }) {
 	return (
 		<article>
-			<h2 className='text-Off_white text-xl'>{titulo}</h2>
+			<h2 className='text-Off_white text-xl hover:text-Soft_Orange hover:cursor-pointer'>
+				{titulo}
+			</h2>
 			<p className='text-Grayish_blue'>{desc}</p>
 		</article>
 	)
