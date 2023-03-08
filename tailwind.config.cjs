f9efbee52f854e943f376f87a0d0294fd6d4fcbe
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 10:13:14 -0300

    add color variables

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        Soft_Orange: 'hsl(35, 77%, 62%)',
+        Soft_Red: 'hsl(5, 85%, 63%)',
+        Off_white: 'hsl(36, 100%, 99%)',
+        Grayish_blue: 'hsl(233, 8%, 79%)',
+        Dark_grayish_blue: 'hsl(236, 13%, 42%)',
+        Very_dark_blue: 'hsl(240, 100%, 5%)',
+      },
+    },
   },
   plugins: [],
 }
