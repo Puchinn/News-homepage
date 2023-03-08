3cc17ae6407887e7d6fbb9490a67ad87bb1a85c6
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 17:57:17 -0300

    add data

---
+++ src/data/items.js
@@ -0,0 +1,23 @@
+const items = [
+    {
+        id: "01",
+        title: "Reviving Retro PCs",
+        desc: "What happens when old PCs are given modern upgrades?",
+        image: "src/assets/images/image-retro-pcs.jpg"
+
+    },
+    {
+        id: "02",
+        title: "Top 10 Laptops of 2022",
+        desc: "Our best picks for various needs and budgets.",
+        image: "src/assets/images/image-top-laptops.jpg"
+    },
+    {
+        id: "03",
+        title: "The Growth of Gaming",
+        desc: "How the pandemic has sparked fresh opportunities.",
+        image: "src/assets/images/image-gaming-growth.jpg"
+    }
+]
+
+export default items
