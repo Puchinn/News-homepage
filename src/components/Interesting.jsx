5b80a209a79cf2d639b4b4436a65c90e445c83b7
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 18:02:49 -0300

    create component

---
+++ src/components/Interesting.jsx
@@ -0,0 +1,22 @@
+import items from '../data/items'
+import Item from './Item'
+
+function Interesting() {
+	return (
+		<section className='col-span-12 grid grid-cols-12'>
+			{items.map(item => {
+				return (
+					<Item
+						key={item.id}
+						image={item.image}
+						id={item.id}
+						title={item.title}
+						desc={item.desc}
+					/>
+				)
+			})}
+		</section>
+	)
+}
+
+export default Interesting
