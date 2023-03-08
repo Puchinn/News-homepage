793fa2e716649742ba84503320a603dda4c6833f
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 20:59:16 -0300

    update styles

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -5,14 +5,16 @@ function MainContent() {
 		<section>
 			<img src={image} alt='web-3-image' />
 			<div className='flex w-full pt-6'>
-				<h1 className='text-6xl font-bold flex-1'>The Bright Future of Web 3.0?</h1>
+				<h1 className='text-6xl font-bold flex-1 text-Very_dark_blue'>
+					The Bright Future of Web 3.0?
+				</h1>
 				<div className='px-4 space-y-10 flex-1'>
-					<p>
+					<p className='text-Dark_grayish_blue'>
 						We dive into the next evolution of the web that claims to put the power of the
 						platforms back into the hands of the people. But is it really fulfilling its
 						promise?
 					</p>
-					<button className='py-2 px-7 text-white bg-Soft_Red hover:bg-Very_dark_blue'>
+					<button className='py-3 px-7 text-white bg-Soft_Red hover:bg-Very_dark_blue'>
 						<span className='tracking-[0.2rem] font-semibold'>READ MORE</span>
 					</button>
 				</div>
