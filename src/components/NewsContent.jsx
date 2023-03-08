6f59377ad2e36f19e13d67dcb32a04420b105035
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 18:05:47 -0300

    add iteration to render Article component

--- src/components/NewsContent.jsx
+++ src/components/NewsContent.jsx
@@ -1,10 +1,13 @@
 import Article from './Article'
+import news from '../data/news'
 
 function NewsContent() {
 	return (
 		<section className='p-5 bg-Very_dark_blue'>
 			<h1 className='text-4xl font-semibold text-Soft_Orange'>New</h1>
-			<Article titulo={'Hydrogen VS Electric Cars'} />
+			{news.map(item => {
+				return <Article key={item.title} titulo={item.title} desc={item.desc} />
+			})}
 		</section>
 	)
 }
