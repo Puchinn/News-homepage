e1ebf5aca7f65f1173b17e72946b6289c174899e
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 14:11:03 -0300

    create component

---
+++ src/components/Article.jsx
@@ -0,0 +1,10 @@
+function Article({ titulo, desc }) {
+	return (
+		<article>
+			<h2 className='text-Off_white text-xl'>{titulo}</h2>
+			<p className='text-Grayish_blue'>{desc}</p>
+		</article>
+	)
+}
+
+export default Article
---
+++ src/components/NewsContent.jsx
@@ -0,0 +1,12 @@
+import Article from './Article'
+
+function NewsContent() {
+	return (
+		<section className='p-5 bg-Very_dark_blue'>
+			<h1 className='text-4xl font-semibold text-Soft_Orange'>New</h1>
+			<Article titulo={'Hydrogen VS Electric Cars'} />
+		</section>
+	)
+}
+
+export default NewsContent
