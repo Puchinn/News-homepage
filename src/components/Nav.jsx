c98d3f88cb87f1cb9c422498f2b2769872968e13
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 09:49:37 -0300

    create component folder

---
+++ src/components/Nav.jsx
@@ -0,0 +1,16 @@
+function Nav() {
+	return (
+		<header className='flex justify-between max-w-7xl mx-auto items-end'>
+			<h1 className='text-6xl font-bold'>W.</h1>
+			<nav className='space-x-6 text-gray-600'>
+				<a href='#'>Home</a>
+				<a href='#'>New</a>
+				<a href='#'>Popular</a>
+				<a href='#'>Trending</a>
+				<a href='#'>Categories</a>
+			</nav>
+		</header>
+	)
+}
+
+export default Nav
