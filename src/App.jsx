baed7fda9a8ca5f693066d0e09e147d0a46e4d2d
Esteban Sayago <sayagoo823@gmail.com>
2023-03-08 21:00:21 -0300

    update styles of layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,16 @@ import Interesting from './components/Interesting'
 
 function App() {
 	return (
-		<div className='App max-w-7xl mx-auto space-y-7'>
-			<Nav />
-			<div className='grid grid-cols-12 gap-x-7'>
-				<div className='col-span-8'>
-					<MainContent />
-				</div>
-				<div className='col-span-4'>
-					<NewsContent />
+		<div className='App bg-Off_white py-3'>
+			<div className='max-w-5xl mx-auto space-y-10 font-inter'>
+				<Nav />
+				<div className='grid grid-cols-12 gap-x-7 py-4'>
+					<div className='col-span-8'>
+						<MainContent />
+					</div>
+					<div className='col-span-4'>
+						<NewsContent />
+					</div>
 				</div>
 				<Interesting />
 			</div>
