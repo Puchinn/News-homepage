c2f479d36d2cfaf66da382b90418fd6c49ba702a
Esteban Sayago <sayagoo823@gmail.com>
2023-03-09 16:40:25 -0300

    create component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+function Footer() {
+	return (
+		<div className='attribution text-center pt-12'>
+			Challenge by{' '}
+			<a
+				className='text-blue-600 underline'
+				href='https://www.frontendmentor.io?ref=challenge'
+				rel='noreferrer'
+				target='_blank'>
+				Frontend Mentor
+			</a>
+			. Coded by{' '}
+			<a
+				className='text-blue-600 underline'
+				rel='noreferrer'
+				target='_blank'
+				href='https://github.com/Puchinn'>
+				Esteban Sayago
+			</a>
+			.
+		</div>
+	)
+}
+
+export default Footer
