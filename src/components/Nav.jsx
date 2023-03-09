0e0ebad48a1c5b0feac87b654b0a8cb7a488d689
Esteban Sayago <sayagoo823@gmail.com>
2023-03-09 11:18:10 -0300

    add state to menu responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,29 @@
+import ButtonMenu from './ButtonMenu'
+import { useState } from 'react'
+
 function Nav() {
+	const [isActive, setIsActive] = useState(false)
+
 	return (
-		<header className='flex justify-between max-w-7xl mx-auto items-end'>
-			<h1 className='text-6xl font-bold text-Very_dark_blue'>W.</h1>
-			<nav className='space-x-6 text-Dark_grayish_blue [&_>a:hover]:text-Soft_Red'>
+		<header className='flex flex-col justify-between max-w-7xl mx-auto md:items-end'>
+			<div className='flex justify-between'>
+				<h1 className='text-5xl font-bold text-Very_dark_blue'>W.</h1>
+				<ButtonMenu isActive={isActive} setIsActive={setIsActive} />
+			</div>
+
+			{isActive && (
+				<div className='fixed left-0 w-screen h-screen top-0 grid grid-cols-5 bg-Very_dark_blue bg-opacity-50'>
+					<div className='col-span-2'></div>
+					<div className='flex flex-col text-Very_dark_blue col-span-3 gap-y-3 pl-3 bg-Off_white py-32 [&_>a:hover]:text-Soft_Red'>
+						<a href='#'>Home</a>
+						<a href='#'>New</a>
+						<a href='#'>Popular</a>
+						<a href='#'>Trending</a>
+						<a href='#'>Categories</a>
+					</div>
+				</div>
+			)}
+			<nav className='space-x-6 hidden text-Dark_grayish_blue [&_>a:hover]:text-Soft_Red'>
 				<a href='#'>Home</a>
 				<a href='#'>New</a>
 				<a href='#'>Popular</a>
